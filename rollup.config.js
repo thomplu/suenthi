5eeb1e91681b2a412c395c1c29914618e94a7856
plusklang <thomas.plucinski@yahoo.de>
2022-03-23 12:35:14 +0100

    Remove dist folder since github pages works without it

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from '@rollup/plugin-node-resolve';
 import scss from 'rollup-plugin-scss';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
-import copy from 'rollup-plugin-copy'
+// import resolve from '@rollup/plugin-node-resolve';
+// import copy from 'rollup-plugin-copy'
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -20,13 +20,13 @@ export default {
             output: 'main.css',
             watch: 'main.scss'
         }),
-        copy({
-            targets: [
-                { src: 'index.html', dest: 'dist' },
-                { src: 'script.js', dest: 'dist' },
-                { src: 'main.css', dest: 'dist' },
-            ]
-        }),
+        // copy({
+        //     targets: [
+        //         { src: 'index.html', dest: 'dist' },
+        //         { src: 'script.js', dest: 'dist' },
+        //         { src: 'main.css', dest: 'dist' },
+        //     ]
+        // }),
         !production &&
         (serve({
             contentBase: '.',
